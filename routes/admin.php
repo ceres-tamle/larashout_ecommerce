66db8628075baafd5d87da0022e161baa407b53f
LeDucTam-1102 <108506234+tamletomia@users.noreply.github.com>
2022-09-09 14:06:35 +0700

    login logout

--- routes/admin.php
+++ routes/admin.php
@@ -15,14 +15,3 @@ Route::group(['prefix' => 'admin'], function () {
     Route::post('login', [App\Http\Controllers\Admin\LoginController::class, 'login'])->name('admin.login.post');
     Route::get('logout', [App\Http\Controllers\Admin\LoginController::class, 'logout'])->name('admin.logout');
 });
-
-// Route::group(['prefix'  =>  'admin'], function () {
-
-//     Route::get('/', function () {
-//         return view('admin.dashboard.index');
-//     });
-
-//     Route::get('login', 'Admin\LoginController@showLoginForm')->name('admin.login');
-//     Route::post('login', 'Admin\LoginController@login')->name('admin.login.post');
-//     Route::get('logout', 'Admin\LoginController@logout')->name('admin.logout');
-// });
